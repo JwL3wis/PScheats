2905e7ebc213d05d93ca821014d4932acb0529dc
JwL3wis <118464892+JwL3wis@users.noreply.github.com>
2023-05-15 17:48:41 +0100

    Add files via upload

---
+++ Remove Local Admin/RemoveLocalAdmin.ps1
@@ -0,0 +1,33 @@
+﻿Read-Host This script will remove users from the Administrators group on a computer.
+Read-Host Winthin the directory you are running this script from you must name a CSV file call computers.csv that has two columns REMOTE_COMPUTER_NAME and USER_NAME. 
+
+$computers = Import-Csv "$PSScriptRoot\inputlist.csv"
+$outputFile = "$PSScriptRoot\output.txt"
+
+$successCount = 0
+$failureCount = 0
+
+foreach ($computer in $computers) {
+    $remoteComputerName = $computer.REMOTE_COMPUTER_NAME
+    $userName = $computer.USER_NAME
+
+    try {
+        Invoke-Command -ComputerName $remoteComputerName -ScriptBlock {
+            Remove-LocalGroupMember -Group "Administrators" -Member $using:userName
+        }
+        $status = "Success"
+        $successCount++
+    }
+    catch {
+        $status = "Failed: $_"
+        $failureCount++
+    }
+
+    $output = "$remoteComputerName,$userName,$status"
+    $output | Out-File -FilePath $outputFile -Append
+}
+
+Write-Host "Number of successes: $successCount"
+Write-Host "Number of failures: $failureCount"
+Write-Host "See output.txt"
+Read-Host -Prompt "Press Enter to exit"
