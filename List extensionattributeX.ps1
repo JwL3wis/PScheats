61d6fc4fc1afa55d7a4541698e0f2cbcae71b05c
JwL3wis <118464892+JwL3wis@users.noreply.github.com>
2023-05-16 11:43:22 +0100

    Update List extensionattributeX.ps1

--- List extensionattributeX.ps1
+++ List extensionattributeX.ps1
@@ -1,11 +1,15 @@
-﻿# Replace with the distinguished names of the OUs you want to search, separated by commas
-$ous = "OU=Users1,DC=example,DC=com","OU=Users2,DC=example,DC=com" 
+# Define an array of OUs to search for inactive users
+# Replace with the distinguished names of the OUs you want to search, separated by commas
+$searchBases = @('OU=Users1,DC=example,DC=com', 'OU=Users2,DC=example,DC=com')
 
-$results = foreach ($ou in $ous) {
-    Get-ADUser -Filter * -SearchBase $ou -Properties extensionattributeX, LastLogonDate | Select-Object Name, CanonicalName, SamAccountName, extensionattribute8, LastLogonDate, Enabled, UserPrincipalName,DistinguishedName
+$results = foreach ($searchBase in $searchBases) {
+    Get-ADUser -Filter * -SearchBase $searchBase -Properties extensionattribute8, LastLogonDate |
+        Where-Object { $_.extensionattributeX -ne $null } |
+        Select-Object Name, CanonicalName, SamAccountName, extensionattribute8, LastLogonDate, Enabled, UserPrincipalName,DistinguishedName
 }
+
 $count = $results.Count
 Write-Host "$count users found"
 $results | Export-Csv -Path "C:\temp\output.csv" -NoTypeInformation
-Write-Host "Output has been exported to 'C:\Temp\Output.csv'."
+Write-Host "Output has been exported to 'C:\Temp\output.csv'."
 Read-Host -Prompt "Press Enter to exit"
