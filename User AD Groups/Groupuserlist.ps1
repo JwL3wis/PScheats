8c3bdc73b51c640d8a3ca436bdcc2f9586955e41
JwL3wis <118464892+JwL3wis@users.noreply.github.com>
2023-05-16 17:50:15 +0100

    Add files via upload

---
+++ User AD Groups/Groupuserlist.ps1
@@ -0,0 +1,15 @@
+﻿$userlist = Get-Content -Path "$PSScriptRoot\userlist.txt"
+$groupData = @()
+
+foreach ($user in $userlist) {
+    Write-Host "Getting group membership for $user"
+    $groups = Get-ADPrincipalGroupMembership -Identity $user | Select-Object Name
+    $groupData += [PSCustomObject] @{
+        User = $user
+        Groups = ($groups | Select-Object -ExpandProperty Name) -join ","
+    }
+}
+
+$groupData | Export-Csv -Path "$PSScriptRoot\user_groups.csv" -NoTypeInformation
+Write-Host "Report has been exported to 'user_groups.csv'."
+Read-Host -Prompt "Press Enter to exit"
