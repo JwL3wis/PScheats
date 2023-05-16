81eefdc99a162243b623a10f1baf4681ca718751
JwL3wis <118464892+JwL3wis@users.noreply.github.com>
2023-05-16 11:21:20 +0100

    Add files via upload

---
+++ List extensionattributeX.ps1
@@ -0,0 +1,11 @@
+﻿# Replace with the distinguished names of the OUs you want to search, separated by commas
+$ous = "OU=Users1,DC=example,DC=com","OU=Users2,DC=example,DC=com" 
+
+$results = foreach ($ou in $ous) {
+    Get-ADUser -Filter * -SearchBase $ou -Properties extensionattribute8, LastLogonDate | Select-Object Name, CanonicalName, SamAccountName, extensionattribute8, LastLogonDate, Enabled, UserPrincipalName,DistinguishedName
+}
+$count = $results.Count
+Write-Host "$count users found"
+$results | Export-Csv -Path "C:\temp\output.csv" -NoTypeInformation
+Write-Host "Output has been exported to 'C:\Temp\Inactive Users 6m.csv'."
+Read-Host -Prompt "Press Enter to exit"
