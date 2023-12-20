f12f2bf84b739354e220ca5599bee99f64b3a9c0
JwL3wis <118464892+JwL3wis@users.noreply.github.com>
2023-12-20 10:55:20 +0000

    Add files via upload

---
+++ ADLogonto.ps1
@@ -0,0 +1,18 @@
+﻿$Usernames = @("User1", "User2")
+
+foreach ($Username in $Usernames) {
+    # Get the AD user object
+    $User = Get-ADUser -Identity $Username -Properties LogonWorkstations
+
+    # Check if the 'LogonWorkstations' attribute is set
+    if ($User.LogonWorkstations -ne $null) {
+        # The 'LogonWorkstations' attribute contains the list of allowed computers
+        Write-Host "Computers that $Username is allowed to log on to:"
+        $User.LogonWorkstations
+    } else {
+        Write-Host "The user $Username is not restricted from logging on to specific computers."
+    }
+
+    # Add a separator between user results
+    Write-Host "------------------------"
+}
